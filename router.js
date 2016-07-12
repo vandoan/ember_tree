4e077f00a045948d98c5df04805565e6bdffd055
Van Doan <vdoan09@gmail.com>
2016-07-11 22:29:23 -0400

    initial commit

---
+++ router.js
@@ -0,0 +1,9 @@
+Blogger.Router.map(function(){
+	this.resource('posts', { path: "/"});
+	this.resource('about');
+	this.resource('contact', function(){
+		this.resource('phone');
+		this.resource('email');
+	});
+	
+});
